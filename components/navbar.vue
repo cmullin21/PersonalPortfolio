6c969d6f119abda9c10959bc6241dda3a7554348
Cody Mullin <cmullin21@gmail.com>
2020-02-07 17:07:19 -0500

    Updated Navbar size

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,11 +2,11 @@
       <!-- navbar -->
       <div class="container-about flex ml-auto mr-auto justify-between">
         <div>
-          <h1 class="font header text-lg md:text-2xl text-center pt-8"><nuxt-link to="/"><span class="firstName">cody</span>mullin</nuxt-link></h1>
+          <h1 class="font header text-sm md:text-2xl text-center pt-8"><nuxt-link to="/"><span class="firstName">cody</span>mullin</nuxt-link></h1>
         </div>
         <div class="md:flex-shrink-0 flex">
-          <h1 class="font header text-lg md:text-2xl text-center pt-8 px-4"><nuxt-link to="/projects">projects</nuxt-link></h1>
-          <h1 class="font header text-lg md:text-2xl text-center pt-8"><a href="https://www.behance.net/CodyMullinProjects">behance</a></h1>
+          <h1 class="font header text-sm md:text-2xl text-center pt-8 px-4"><nuxt-link to="/projects">projects</nuxt-link></h1>
+          <h1 class="font header text-sm md:text-2xl text-center pt-8"><a href="https://www.behance.net/CodyMullinProjects">behance</a></h1>
         </div>
       </div>
 </template>
